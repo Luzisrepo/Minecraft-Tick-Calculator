cb66d3760d293be771df8f52699af3bfae44636d
._rayzz / luzisrepo <andrecunhamartins.w122@gmail.com>
2025-08-11 15:03:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 function calculate() {
-  var outputNumber1 = document.getElementById('seconds').value *(20) + 
+  var seconds = Number(document.getElementById('seconds').value) || 0;
+  var minutes = Number(document.getElementById('minutes').value) || 0;
+  var hours = Number(document.getElementById('hours').value) || 0;
+  var days = Number(document.getElementById('days').value) || 0;
+  var years = Number(document.getElementById('years').value) || 0;
 
-document.getElementById('seconds').value * 20 +      
-document.getElementById('minutes').value *(60 * 20) + 
-document.getElementById('hours').value *(60 * 60 * 20) + 
-document.getElementById('days').value *(60 * 60 * 24 * 20) + 
-document.getElementById('years').value *(60 * 60 * 24 * 365 * 20);
+  var outputNumber1 = 
+    seconds * 20 +
+    minutes * 60 * 20 +
+    hours * 60 * 60 * 20 +
+    days * 60 * 60 * 24 * 20 +
+    years * 60 * 60 * 24 * 365 * 20;
 
-document.getElementById('output1').value = parseInt(outputNumber1);
+  document.getElementById('output1').value = parseInt(outputNumber1);
 }
 
 function myFunction() {
-  
-    document.getElementById('seconds').value = "";
-    document.getElementById('minutes').value = ""; 
-    document.getElementById('hours').value = ""; 
-    document.getElementById('days').value = ""; 
-    document.getElementById('years').value = "";
-    document.getElementById('output1').value = "";
+  document.getElementById('seconds').value = "";
+  document.getElementById('minutes').value = ""; 
+  document.getElementById('hours').value = ""; 
+  document.getElementById('days').value = ""; 
+  document.getElementById('years').value = "";
+  document.getElementById('output1').value = "";
 }
